9ded9d38a642d0a6223efbfef73b0206cc67860b
Josh Orndorff <JoshOrndorff@users.noreply.github.com>
2014-05-16 10:37:22 -0400

    Update triangle2.py

--- triangle2.py
+++ triangle2.py
@@ -1,1 +1,1 @@
-HA! You really thought I would put my answer to your homework here!? Get back to work punk, and ask me if you need help.
+HA! You really thought I would put my answer to your homework here!? Get back to work punk, and ask me if you need help :)
